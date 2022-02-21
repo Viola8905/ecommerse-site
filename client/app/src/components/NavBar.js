10e21cd0e3bf1a768d8da3dd20f03c994dc84499
Viola8905 <viola89055@gmail.com>
2022-02-21 12:20:48 +0200

    added useNavigate and proper navigate  in NavBar

--- client/app/src/components/NavBar.js
+++ client/app/src/components/NavBar.js
@@ -5,14 +5,16 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import {observer} from 'mobx-react-lite';
-
+import { useNavigate } from "react-router-dom";
 
 
 
 const NavBar = observer (() => {
   const { user } = useContext(Context);
+	const navigate = useNavigate();
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -22,16 +24,30 @@ const NavBar = observer (() => {
           </NavLink>
           {user.isAuth ? (
             <Nav className="ml-auto" style={{ color: "white" }}>
-              <Button variant={"outline-light"}>Админ панель</Button>
-              <Button variant={"outline-light"} className='ml-2'>Войти</Button>
+              <Button
+                variant={"outline-light"}
+                onClick={() => navigate(ADMIN_ROUTE)}
+              >
+                Админ панель
+              </Button>
+              <Button
+                variant={"outline-light"}
+                className="ml-2"
+                onClick={() => navigate(LOGIN_ROUTE)}
+              >
+                Выйти
+              </Button>
             </Nav>
           ) : (
             <Nav className="ml-auto" style={{ color: "white" }}>
-              
-              <Button variant={"outline-light"} onClick = {() => user.setIsAuth(true)}>Авторизация</Button>
+              <Button
+                variant={"outline-light"}
+                onClick={() => user.setIsAuth(true)}
+              >
+                Авторизация
+              </Button>
             </Nav>
           )}
-         
         </Container>
       </Navbar>
     </>
